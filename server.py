94dbfa3bb1b91058544cf6f2662fc4a542c2458f
Joseph <joey@pop-os.localdomain>
2022-04-25 17:31:16 +1000

    added more client crap

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import logging
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,8 +15,10 @@ class ChatServer(Resource):
         return messages
     def put(self, author):
         messages[author] = request.form['content']
+        print(f'{author}: {messages[author]}')
         return {author: messages[author]}
 
 api.add_resource(ChatServer, '/<string:author>', '/buddy')
+
 if __name__ == '__main__':
     app.run(debug=True)
